a51ebb19613e4fba709209e4c061a86aab47f686
出川　大和 <hxdegawa@gmail.com>
2019-05-05 14:48:41 +0900

    Fixed charCode problem by replacing crawling module

--- app.js
+++ app.js
@@ -1,43 +1,73 @@
 const Discord = require('discord.js');
+const request = require('request-promise');
 const client = new Discord.Client();
-const request = require('request');
-require('dotenv').config()
+const metameta = require('riassumere').default;
+require('dotenv').config();
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
 
-const options = {
-  uri: process.env.GGL_TOKEN,
-  headers: {
-    "Content-type": "application/json",
-  },
-  json: {
-    "dynamicLinkInfo": {
-      "domainUriPrefix": "https://ori.page.link",
-      "link": ""
-    }
+client.on('message', async message => {
+  const fullContents = message.content.trim().split(" ");
+  
+  if (!isUrl(fullContents[0]) || fullContents[0].length <= 40 || message.author.bot) {
+    return false;
   }
-};
-
-client.login(process.env.BOT_TOKEN);
-
-client.on('message', message => {
-  if(((message.content.trim().slice(0, 7) === "https:/" || message.content.trim().slice(0, 7) === "http://")) && message.content.length >= 40){
-    options.json.dynamicLinkInfo.link = message.content.split(" ")[0];
-    request.post(options, function(error, response, body){
-      if(error){
-        return false;
-      }
-
-      if(body && message.channel.guild !== undefined){
-        message.delete();
-        message.channel.send(body.shortLink);
-      }else if(body && message.channel.guild === undefined){
-        message.channel.send(body.shortLink);
-      }
-    });
-  }
+  
+  request(createRequestJson(fullContents[0]))
+  .then(data => replaceMessage(data.shortLink, message, fullContents[0]))
+  .catch(console.error);
 });
 
-client.on('error', console.error);
+async function replaceMessage(shortLink, msg, originalLink)  {
+  const embedMessage = new Discord.RichEmbed();
+
+  msg.delete();
+
+  metameta(originalLink)
+  .then(metadata => {
+
+    embedMessage
+    .setAuthor(msg.author.username, msg.author.avatarURL)
+    .setDescription(metadata.description)
+    .setURL(shortLink);
+    metadata.image ? embedMessage.setThumbnail(metadata.image) : null;
+  })
+  .then(result => {
+
+    if (result) {
+      result.length < 60 ? embedMessage.setTitle(result) : embedMessage.setTitle(result.slice(0, 30) + "...");
+      msg.channel.send(embedMessage);
+    } else {
+      msg.channel.send(embedMessage);
+    }
+
+  })
+  .catch(console.error);
+}
+
+function createRequestJson (link) {
+  return {
+    method: 'POST',
+    uri: `https://firebasedynamiclinks.googleapis.com/v1/shortLinks?key=${process.env.GGL_TOKEN}`,
+    headers: {
+      "Content-type": "application/json",
+    },
+    body: {
+      "dynamicLinkInfo": {
+        "domainUriPrefix": "https://ori.page.link",
+        "link": link
+      }
+    },
+    json: true
+  };
+}
+
+function isUrl(s) {
+  var regexp = /(ftp|http|https):\/\/(\w+:{0,1}\w*@)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%@!\-\/]))?/
+  return regexp.test(s);
+}
+
+client.login(process.env.BOT_TOKEN);
+client.on("error", console.error);
